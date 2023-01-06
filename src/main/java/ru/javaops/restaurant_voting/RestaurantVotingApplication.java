581e768f56c11df57ad5b0c3047764926864ed58
Mikle <themis555@mail.ru>
2023-01-06 10:19:34 +0300

    Init project

---
+++ src/main/java/ru/javaops/restaurant_voting/RestaurantVotingApplication.java
@@ -0,0 +1,13 @@
+package ru.javaops.restaurant_voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javaops/restaurant_voting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javaops.restaurant_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
